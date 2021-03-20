8993f218fd8fbe41c89ddd78d98f0f24e38cf440
Connor <connorwilliams.home@gmail.com>
2021-03-20 19:05:08 +1100

    feature: add multiselect

--- extension/background.js
+++ extension/background.js
@@ -2,102 +2,121 @@ let activated = false;
 window.blobs = [];
 
 const toggleHighlight = () => {
-  chrome.webRequest.onBeforeSendHeaders.removeListener(beforeSendHeaders);
-  // gets the current tab and passes it directly into the toggle function
-  chrome.tabs.query({}, (tabs) => {
-    if (tabs.length > 0) {
-      toggle(tabs);
-    }
-  });
+    // gets the current tab and passes it directly into the toggle function
+    chrome.tabs.query({}, (tabs) => {
+        if (tabs.length > 0) {
+            toggle(tabs);
+        }
+    });
 };
 
-var parent = chrome.contextMenus.create({
-  title: "Toggle Content Saver",
-  onclick: toggleHighlight,
+let parent = chrome.contextMenus.create({
+    title: "Toggle Content Saver",
+    onclick: toggleHighlight,
 });
 
 chrome.runtime.onMessage.addListener((request) => {
-  if (request == "DEACTIVATE") {
-    activated = false;
-    chrome.tabs.query({}, (tabs) => {
-      tabs.forEach((tab) => {
-        chrome.tabs.sendMessage(tab.id, "DEACTIVATE_CONTENT_HIGHLIGHT");
-      });
-    });
-  }
-  if (request.message && request.message == "DATA") {
-    let data = new Set(request.data);
-    console.log("RECEIVED DATA:", data);
-    data.forEach((element) => {
-      if (!element) {
-        return;
-      }
-      if (!element.includes("blob:")) {
-        // special edge case for tiktok
-        if (element.includes("tiktok")) {
-          downloadTikTok(element);
-        } else {
-          chrome.downloads.download({
-            url: element,
-          });
-        }
-      } else {
-        chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.tabs.sendMessage(tab.id, "ERROR_DOWNLOAD_BLOB");
+    if (request == "DEACTIVATE") {
+        activated = false;
+        chrome.tabs.query({}, (tabs) => {
+            tabs.forEach((tab) => {
+                chrome.tabs.sendMessage(tab.id, "DEACTIVATE_CONTENT_HIGHLIGHT");
+            });
         });
-      }
-    });
-  }
+    }
+    if (request.message && request.message == "DATA") {
+        let data = new Set(request.data);
+        data = Array.from(data)
+        console.log("RECEIVED DATA:", data);
+        let promises = data.map(element => downloadItem(element))
+        console.log(promises)
+        Promise.all(promises).then((items) => {
+            console.log(items)
+            removeOnBeforeSendHeaders()
+        })
+    }
 });
 
-const downloadTikTok = (element) => {
-  fetch(element, {
-    method: "GET",
-    credentials: "same-origin",
-  })
-    .then((data) => data.blob())
-    .then((blob) => {
-      window.blobs.push(blob);
-      chrome.downloads.download({ url: URL.createObjectURL(blob) });
+
+const downloadItem = async (element) => {
+    if (!element) {
+        return false;
+    }
+    if (!element.includes("blob:")) {
+        // special edge case for tiktok
+        if (element.includes("tiktok")) {
+            await downloadTikTok(element);
+        } else {
+            chrome.downloads.download({
+                url: element,
+            });
+        }
+    } else {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+            const tab = tabs[0];
+            chrome.tabs.sendMessage(tab.id, "ERROR_DOWNLOAD_BLOB");
+        });
+    }
+    return true
+}
+
+
+const removeOnBeforeSendHeaders = () => {
+    console.log("REMOVING LISTENER")
+    chrome.webRequest.onBeforeSendHeaders.removeListener(beforeSendHeaders);
+}
+
+const addOnBeforeSendHeaders = () => {
+    console.log("ADDING HEADERS")
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+        beforeSendHeaders,
+        {urls: ["*://v16-web.tiktok.com/*"]},
+        ["requestHeaders", "extraHeaders", "blocking"]
+    );
+}
+
+const downloadTikTok = async (element) => {
+    addOnBeforeSendHeaders()
+    await fetch(element, {
+        method: "GET",
+        credentials: "same-origin",
     })
-    .then((blob) => {
-      chrome.webRequest.onBeforeSendHeaders.removeListener(beforeSendHeaders);
-      return blob;
-    })
-    .catch((error) => {
-      console.log(error);
-      chrome.webRequest.onBeforeSendHeaders.removeListener(beforeSendHeaders);
-      return null;
-    });
+        .then((data) => data.blob())
+        .then((blob) => {
+            console.log(blob)
+            chrome.downloads.download({url: URL.createObjectURL(blob)});
+        })
+        .then((blob) => {
+            return blob;
+        })
+        .catch((error) => {
+            console.log(error);
+            return null;
+        });
 };
 
 chrome.commands.onCommand.addListener(function (command) {
-  if (command == "toggle-highlight-content") {
-    toggleHighlight();
-  }
+    if (command == "toggle-highlight-content") {
+        toggleHighlight();
+    }
 });
 
 const beforeSendHeaders = (details) => {
-  details.requestHeaders.push({
-    name: "Referer",
-    value: "https://www.tiktok.com/",
-  });
-  return { requestHeaders: details.requestHeaders };
+    console.log("DETAILS")
+    details.requestHeaders.push({
+        name: "Referer",
+        value: "https://www.tiktok.com/",
+    });
+    return {requestHeaders: details.requestHeaders};
 };
 
 const toggle = (tabs) => {
-  tabs.forEach((tab) => {
-    if (activated) {
-      chrome.tabs.sendMessage(tab.id, "DEACTIVATE_CONTENT_HIGHLIGHT");
-    } else {
-      chrome.webRequest.onBeforeSendHeaders.addListener(
-        beforeSendHeaders,
-        { urls: ["*://v16-web.tiktok.com/*"] },
-        ["requestHeaders", "extraHeaders", "blocking"]
-      );
-      chrome.tabs.sendMessage(tab.id, "ACTIVATE_CONTENT_HIGHLIGHT");
-    }
-  });
-  activated = !activated;
+    tabs.forEach((tab) => {
+        if (activated) {
+            chrome.tabs.sendMessage(tab.id, "DEACTIVATE_CONTENT_HIGHLIGHT");
+        } else {
+            chrome.tabs.sendMessage(tab.id, "ACTIVATE_CONTENT_HIGHLIGHT");
+        }
+    });
+    activated = !activated;
 };
--- extension/content.js
+++ extension/content.js
@@ -2,119 +2,191 @@
 let selectedElements = [];
 
 chrome.runtime.onMessage.addListener((request) => {
-  selectedElements = [];
-  if (request == "ACTIVATE_CONTENT_HIGHLIGHT") {
-    document.addEventListener("mousemove", highlightContent);
-    document.addEventListener("mousedown", getSrcs);
-  }
-  if (request == "DEACTIVATE_CONTENT_HIGHLIGHT") {
-    deactivate("EXTERNAL");
-  }
-  if (request == "ERROR_DOWNLOAD_BLOB") {
-    alert("Could not download blob url, sorry.");
-  }
+    if (request === "ACTIVATE_CONTENT_HIGHLIGHT") {
+        document.addEventListener("mousedown", selectContent);
+        nodeAddedToDom(null)
+        if(!document.body.className.includes(" capture-cursor")){
+            document.body.className += " capture-cursor"
+        }
+        document.addEventListener('DOMNodeInserted', nodeAddedToDom)
+    }
+
+    if (request === "DEACTIVATE_CONTENT_HIGHLIGHT") {
+        deactivate();
+    }
+
+    if (request === "ERROR_DOWNLOAD_BLOB") {
+        alert("Could not download blob url, sorry.");
+    }
 });
 
-const deactivate = (source) => {
-  document.removeEventListener("mousemove", highlightContent);
-  document.removeEventListener("mousedown", getSrcs);
-  if (source == "INTERNAL") {
-    chrome.runtime.sendMessage("DEACTIVATE");
-  }
-  clearSelectedCSS();
+const nodeAddedToDom = (event) => {
+    console.log("NODE ADDED TO DOM")
+    document.querySelectorAll("a").forEach(element => {
+        element.addEventListener("click", preventClicks)
+        element.parentElement.addEventListener("click", preventClicks)
+    })
+}
+
+const preventClicks = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+}
+
+const deactivate = () => {
+    document.removeEventListener("mousedown", selectContent);
+    document.querySelectorAll("a").forEach(element => {
+        element.removeEventListener("click", preventClicks)
+        element.parentElement.removeEventListener("click", preventClicks)
+    })
+    document.body.className = document.body.className.replace(" capture-cursor", "")
+    document.removeEventListener('DOMNodeInserted', nodeAddedToDom)
+    getSrcs()
+    clearSelectedCSS();
 };
 
-const highlightContent = (event) => {
-  var x = event.clientX,
-    y = event.clientY;
-  // on mouse down print out the element with the mouse is currently over
-  var elementsFromP = document.elementsFromPoint(x, y);
-  var filtered = elementsFromP.filter((element) => {
-    switch (element.tagName) {
-      case "VIDEO":
-        return true;
-      case "IMG":
-        return true;
-      case "DIV":
-        return element.style.backgroundImage ? true : false;
-      default:
-        return false;
+
+const selectContent = (event) => {
+    console.log(event)
+    event.stopPropagation()
+    event.preventDefault()
+    let x = event.clientX,
+        y = event.clientY;
+    // on mouse down print out the element with the mouse is currently over
+    let elementsFromP = document.elementsFromPoint(x, y);
+    console.log(elementsFromP)
+    let button = elementsFromP.find(element => {
+        return element.tagName === "BUTTON"
+    })
+    if(!button) {
+        let filtered = elementsFromP.filter((element) => {
+            switch (element.tagName) {
+                case "VIDEO":
+                    return true;
+                case "IMG":
+                    return true;
+                case "DIV":
+                    return !!element.style.backgroundImage;
+                default:
+                    return false;
+            }
+        });
+
+        const notHighlighted = filtered.filter(element => {
+            const parent = element.parentElement;
+            const childOfClass = parent.querySelector(".CONTENT_SAVER_OVERLAY");
+            return !childOfClass
+        })
+
+        console.log("notHighlighted", notHighlighted)
+
+        const highlighted = filtered.filter(element => {
+            const parent = element.parentElement;
+            return parent.querySelector(".CONTENT_SAVER_OVERLAY")
+        })
+
+        console.log("highlighted", highlighted)
+
+        // if item is selected already, toggle it off
+        // if item is not selected, toggle it on
+        highlightContent(notHighlighted)
+        highlighted.forEach(element => {
+            const parent = element.parentElement;
+            const selector = parent.querySelector(".CONTENT_SAVER_OVERLAY")
+            if(selector){
+                selector.parentElement.removeChild(selector);
+            }
+            selectedElements = selectedElements.filter(e => {
+                return !element.contains(e)
+            })
+        })
+        console.log("SELECTED", selectedElements)
     }
-  });
-  var firsts = [
-    filtered.find((element) => {
-      return element.tagName == "VIDEO";
-    }),
-    filtered.find((element) => {
-      return element.tagName == "IMG";
-    }),
-    filtered.find((element) => {
-      return element.tagName != "IMG" && element.tagName != "VIDEO";
-    }),
-  ];
-  console.log("FIRSTS", firsts);
-  filtered = firsts.filter((element) => {
-    return element != null;
-  });
-  clearSelectedCSS();
-  selectedElements = filtered;
-  filtered.forEach((element) => {
-    const parent = element.parentElement;
-    const childOfClass = parent.querySelector(".CONTENT_SAVER_OVERLAY");
-    if (!childOfClass) {
-      let div = document.createElement("span");
-      div.className += "CONTENT_SAVER_OVERLAY";
-      div.style.height = `${element.offsetHeight}px`;
-      div.style.width = `${element.offsetWidth}px`;
-      parent.appendChild(div);
-    }
-  });
+}
+
+const highlightContent = (selected) => {
+
+    let filtered = selected
+
+    const firsts = [
+        filtered.find((element) => {
+            return element.tagName === "VIDEO";
+        }),
+        filtered.find((element) => {
+            return element.tagName === "IMG";
+        }),
+        filtered.find((element) => {
+            return element.tagName !== "IMG" && element.tagName !== "VIDEO";
+        }),
+    ];
+    console.log("FIRSTS", firsts);
+    filtered = firsts.filter((element) => {
+        return element != null;
+    });
+    filtered.map(item => {
+        if (!selectedElements.includes(item)) {
+            return item
+        }
+    })
+    // clearSelectedCSS();
+    selectedElements = [...selectedElements, ...filtered];
+    filtered.forEach((element) => {
+        const parent = element.parentElement;
+        const childOfClass = parent.querySelector(".CONTENT_SAVER_OVERLAY");
+        if (!childOfClass) {
+            let div = document.createElement("span");
+            div.className += "CONTENT_SAVER_OVERLAY";
+            div.style.height = `${element.offsetHeight}px`;
+            div.style.width = `${element.offsetWidth}px`;
+            parent.appendChild(div);
+        }
+    });
+    console.log("SELECTED", selectedElements)
 };
 
 const clearSelectedCSS = () => {
-  let overlays = document.querySelectorAll(".CONTENT_SAVER_OVERLAY");
-  overlays.forEach((element) => {
-    element.parentElement.removeChild(element);
-  });
+    let overlays = document.querySelectorAll(".CONTENT_SAVER_OVERLAY");
+    overlays.forEach((element) => {
+        element.parentElement.removeChild(element);
+    });
 };
 
-const getSrcs = (event) => {
-  event.preventDefault();
-  event.stopPropagation();
-  if (selectedElements.length > 0) {
-    var srcs = selectedElements.map((element) => {
-      if (element.style.backgroundImage) {
-        const pattern = /https?:\/\/(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
-        let re = new RegExp(pattern);
-        let match = element.style.backgroundImage.match(re);
-        if (match && match[0]) {
-          return match[0];
-        }
-      }
-      if (element.currentSrc) {
-        return element.currentSrc;
-      }
-      if (element.src) {
-        if (element.tagName == "VIDEO") {
-          console.log(element);
-        }
-        return element.src;
-      } else {
-        if (element.children && element.children[0]) {
-          return element.children[0].src;
-        }
-      }
-    });
+const getSrcs = () => {
+    console.log("GETTING SRCS")
+    console.log("SELECTED ELEMENTS", selectedElements)
+    if (selectedElements.length > 0) {
+        let srcs = selectedElements.map((element) => {
+            if (element.style.backgroundImage) {
+                const pattern = /https?:\/\/(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+                let re = new RegExp(pattern);
+                let match = element.style.backgroundImage.match(re);
+                if (match && match[0]) {
+                    return match[0];
+                }
+            }
+            if (element.currentSrc) {
+                return element.currentSrc;
+            }
+            if (element.src) {
+                if (element.tagName === "VIDEO") {
+                    console.log(element);
+                }
+                return element.src;
+            } else {
+                if (element.children && element.children[0]) {
+                    return element.children[0].src;
+                }
+            }
+        });
 
-    console.log("SOURCES", srcs);
-
-    if (srcs.length > 0) {
-      chrome.runtime.sendMessage({
-        message: "DATA",
-        source: `${window.location.protocol}//${window.location.hostname}${window.location.pathname}`,
-        data: srcs,
-      });
+        console.log("SOURCES", srcs);
+        selectedElements = []
+        if (srcs.length > 0) {
+            chrome.runtime.sendMessage({
+                message: "DATA",
+                source: `${window.location.protocol}//${window.location.hostname}${window.location.pathname}`,
+                data: srcs,
+            });
+        }
     }
-  }
-  deactivate("INTERNAL");
 };
